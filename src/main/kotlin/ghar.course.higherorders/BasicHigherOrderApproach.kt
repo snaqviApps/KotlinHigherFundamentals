38fb1e46712774ec0d3c3d6e05ea161cdf7eb24a
abbas <naqvi.sdsu@gmail.com>
2022-08-17 22:55:02 -0500

    basic HigherOrder function (that implements some 'strategy')

--- src/main/kotlin/ghar.course.higherorders/BasicHigherOrderApproach.kt
+++ src/main/kotlin/ghar.course.higherorders/BasicHigherOrderApproach.kt
@@ -1,25 +1,37 @@
 package ghar.course.higherorders.entry.ghar.course.higherorders
 
 fun main(args: Array<String>) {
-    println("Hi from basic-higherOrder functions approach: ")
-    BasicHigherOrderApproach.fibonacci(5)
+
+    /**
+     * anonymous object (similar to java anonymous) approach
+     * @process
+     */
+    BasicHigherOrderApproach.fibonacci(5, object : BasicHigherOrderApproach.Companion.Process {
+        override fun execute(value: Int) {
+            println(value)
+        }
+    })
 }
 
 class BasicHigherOrderApproach {
 
     companion object {
 
-        fun fibonacci(limit: Int) {   // withOut HigherOrder approach
+        interface Process {
+            fun execute(value: Int)
+        }
+
+        fun fibonacci(limit: Int, action: Process) {
             var prePrev = 0
             var prev = 0
             var current: Int
-            println(prePrev)
+            action.execute(prev)               // strategy, same
             for (i in 1..limit) {
                 current = prePrev + prev
                 when (current) {
                     0 -> current = 1
                 }
-                println(current)
+                action.execute(current)         // strategy
                 prePrev = prev
                 prev = current
             }
