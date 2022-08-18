d4c2504990212adc58a21abfecfb5b0b8cda451e
abbas <naqvi.sdsu@gmail.com>
2022-08-18 00:20:44 -0500

    basic HigherOrder function, moved to using functional-style

--- src/main/kotlin/ghar.course.higherorders/BasicHigherOrderApproach.kt
+++ src/main/kotlin/ghar.course.higherorders/BasicHigherOrderApproach.kt
@@ -3,39 +3,31 @@ package ghar.course.higherorders.entry.ghar.course.higherorders
 fun main(args: Array<String>) {
 
     /**
-     * anonymous object (similar to java anonymous) approach
-     * @process
+     * moved to 'functional-style'
      */
-    BasicHigherOrderApproach.fibonacci(5, object : BasicHigherOrderApproach.Companion.Process {
-        override fun execute(value: Int) {
-            println(value)
-        }
-    })
+    val basicHigherOrderApproach = BasicHigherOrderApproach()
+//    basicHigherOrderApproach.fibonacci(5, {  println(it) })                    // same as below
+//    basicHigherOrderApproach.fibonacci(5, { value -> println(value) })            // same as below
+    basicHigherOrderApproach.fibonacci(5) { value -> println(value) }             // same as below: moved above lambda 'outside of parenthesis'
+//    basicHigherOrderApproach.fibonacci(5, ::println)                              // same as above, but using functional-reference
 }
 
 class BasicHigherOrderApproach {
 
-    companion object {
-
-        interface Process {
-            fun execute(value: Int)
-        }
-
-        fun fibonacci(limit: Int, action: Process) {
+        fun fibonacci(limit: Int, action: (Int) -> Unit) {
             var prePrev = 0
             var prev = 0
             var current: Int
-            action.execute(prev)               // strategy, same
+            action(prev)               // strategy, same as below
             for (i in 1..limit) {
                 current = prePrev + prev
                 when (current) {
                     0 -> current = 1
                 }
-                action.execute(current)         // strategy
+                action(current)         // strategy
                 prePrev = prev
                 prev = current
             }
         }
 
-    }
 }
